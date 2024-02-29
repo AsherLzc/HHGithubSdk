ae9a4f0682803d25f455088009b3217af892ba97
lizhichen <lizhichen@hh-medic.com>
2024-02-29 19:08:53 +0800

    api

--- app/src/main/java/com/hhmedic/githubsdk/MainActivity.kt
+++ app/src/main/java/com/hhmedic/githubsdk/MainActivity.kt
@@ -2,10 +2,10 @@ package com.hhmedic.githubsdk
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.hhmedic.android.sdk.HHDoctor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 }
